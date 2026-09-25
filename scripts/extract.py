be88472555c3c72c0fa00e1a336e54474ac6fa53
Yoann Robert <yoann.robert44@gmail.com>
2026-09-25 16:33:46 +0200

    Put code into functions for extract and transform steps

--- scripts/extract.py
+++ scripts/extract.py
@@ -14,23 +14,30 @@ from access_it.etl.extract.races import (
 )
 from access_it.etl.extract.clubs import get_committees, get_clubs
 
-with make_client() as client:
-    for dept in [
-        44, 49, 53, 85, 72,  # Pays de La Loire
-        22, 29, 35, 56,  # Bretagne
-    ]:
-        extract_organization_pages_from_search_url(client=client, dept=dept)
-    extract_former_organization_pages_from_existing_ones(client=client)
-    extract_former_organization_pages_using_bruteforce(client=client)
 
-    reg_committees, dep_committees = get_committees(client)
-    clubs = get_clubs(client, departemental_committees=dep_committees)
-    this_year = datetime.now().year
-    clubs["min_year"] = this_year
-    clubs["max_year"] = this_year
-    clubs["alternative_names"] = ""
-    departements = get_departement_mapping(client)
-    pd.DataFrame(reg_committees).to_parquet(REGCOM_PARQUET_FILE, index=False)
-    pd.DataFrame(dep_committees).to_parquet(DEPCOM_PARQUET_FILE, index=False)
-    pd.DataFrame(clubs).to_parquet(CLUBS_PARQUET_FILE, index=False)
-    pd.DataFrame(departements).to_parquet(DEPARTEMENTS_PARQUET_FILE, index=False)
+def extract():
+
+    with make_client() as client:
+        for dept in [
+            44, 49, 53, 85, 72,  # Pays de La Loire
+            22, 29, 35, 56,  # Bretagne
+        ]:
+            extract_organization_pages_from_search_url(client=client, dept=dept)
+        extract_former_organization_pages_from_existing_ones(client=client)
+        extract_former_organization_pages_using_bruteforce(client=client)
+
+        reg_committees, dep_committees = get_committees(client)
+        clubs = get_clubs(client, departemental_committees=dep_committees)
+        this_year = datetime.now().year
+        clubs["min_year"] = this_year
+        clubs["max_year"] = this_year
+        clubs["alternative_names"] = ""
+        departements = get_departement_mapping(client)
+        pd.DataFrame(reg_committees).to_parquet(REGCOM_PARQUET_FILE, index=False)
+        pd.DataFrame(dep_committees).to_parquet(DEPCOM_PARQUET_FILE, index=False)
+        pd.DataFrame(clubs).to_parquet(CLUBS_PARQUET_FILE, index=False)
+        pd.DataFrame(departements).to_parquet(DEPARTEMENTS_PARQUET_FILE, index=False)
+
+
+if __name__ == "__main__":
+    extract()
--- scripts/transform.py
+++ scripts/transform.py
@@ -19,51 +19,57 @@ from access_it.etl.transform.clubs import add_clubs_found_in_race_html_files
 from access_it.etl.transform.races import get_race_data, create_race_table
 
 
-# Reading Parquet files obtained in the extract step of the ETL
-departements = pd.read_parquet(DEPARTEMENTS_PARQUET_FILE)
-departemental_committees = pd.read_parquet(DEPCOM_PARQUET_FILE)
-clubs = pd.read_parquet(CLUBS_PARQUET_FILE)
+def transform():
 
-# Enriching club database
-clubs = add_legacy_clubs(
-    df_clubs=clubs,
-    departemental_committees=departemental_committees
-)
-clubs = add_clubs_found_in_race_html_files(
-    clubs,
-    departemental_committees=departemental_committees
-)
+    # Reading Parquet files obtained in the extract step of the ETL
+    departements = pd.read_parquet(DEPARTEMENTS_PARQUET_FILE)
+    departemental_committees = pd.read_parquet(DEPCOM_PARQUET_FILE)
+    clubs = pd.read_parquet(CLUBS_PARQUET_FILE)
 
-# Getting race data
-races_data = get_race_data()  # includes rankings
-races = create_race_table(races_data, departements)  # fewer columns
+    # Enriching club database
+    clubs = add_legacy_clubs(
+        df_clubs=clubs,
+        departemental_committees=departemental_committees
+    )
+    clubs = add_clubs_found_in_race_html_files(
+        clubs,
+        departemental_committees=departemental_committees
+    )
 
-# Creating intermediate data: rider x race
-# ---- flattening rankings: one line per ranking element
-rider_x_race_data = split_race_data_into_rider_data(races_data)
-# ---- multiple corrections
-rider_x_race_data = correct_wrong_uci_ids(rider_x_race_data)
-rider_x_race_data = add_missing_individual_labels(rider_x_race_data)
-rider_x_race_data = add_missing_foreign_labels(rider_x_race_data)
-# ---- identifying the club of the riders
-rider_x_race_data = identify_club_of_riders(rider_x_race_data, clubs)
-# ---- filling missing data
-rider_x_race_data = pd.DataFrame(rider_x_race_data)
-rider_x_race_data = fill_missing_club_and_uci_ids(rider_x_race_data)
-rider_x_race_data = fill_missing_uci_ids_deep_mode(rider_x_race_data)
+    # Getting race data
+    races_data = get_race_data()  # includes rankings
+    races = create_race_table(races_data, departements)  # fewer columns
 
-# Creating rider database
-rider_db = add_riders_to_database(rider_x_race_data=rider_x_race_data)
+    # Creating intermediate data: rider x race
+    # ---- flattening rankings: one line per ranking element
+    rider_x_race_data = split_race_data_into_rider_data(races_data)
+    # ---- multiple corrections
+    rider_x_race_data = correct_wrong_uci_ids(rider_x_race_data)
+    rider_x_race_data = add_missing_individual_labels(rider_x_race_data)
+    rider_x_race_data = add_missing_foreign_labels(rider_x_race_data)
+    # ---- identifying the club of the riders
+    rider_x_race_data = identify_club_of_riders(rider_x_race_data, clubs)
+    # ---- filling missing data
+    rider_x_race_data = pd.DataFrame(rider_x_race_data)
+    rider_x_race_data = fill_missing_club_and_uci_ids(rider_x_race_data)
+    rider_x_race_data = fill_missing_uci_ids_deep_mode(rider_x_race_data)
 
-# Creating ranking database
-rankings = create_ranking_table(rider_x_race_data, rider_db)
+    # Creating rider database
+    rider_db = add_riders_to_database(rider_x_race_data=rider_x_race_data)
 
-# Creating the club-rider affiliation database
-aff_club_rider = create_affiliation_clubs_riders(rider_x_race_data, races, rider_db)
+    # Creating ranking database
+    rankings = create_ranking_table(rider_x_race_data, rider_db)
 
-# Saving all data to Parquet files
-pd.DataFrame(clubs).to_parquet(CLUBS_PARQUET_FILE, index=False)
-pd.DataFrame(races).to_parquet(RACES_PARQUET_FILE, index=False)
-pd.DataFrame(rider_db).to_parquet(RIDERS_PARQUET_FILE, index=False)
-pd.DataFrame(rankings).to_parquet(RANKINGS_PARQUET_FILE, index=False)
-pd.DataFrame(aff_club_rider).to_parquet(AFFILIATIONS_PARQUET_FILE, index=False)
+    # Creating the club-rider affiliation database
+    aff_club_rider = create_affiliation_clubs_riders(rider_x_race_data, races, rider_db)
+
+    # Saving all data to Parquet files
+    pd.DataFrame(clubs).to_parquet(CLUBS_PARQUET_FILE, index=False)
+    pd.DataFrame(races).to_parquet(RACES_PARQUET_FILE, index=False)
+    pd.DataFrame(rider_db).to_parquet(RIDERS_PARQUET_FILE, index=False)
+    pd.DataFrame(rankings).to_parquet(RANKINGS_PARQUET_FILE, index=False)
+    pd.DataFrame(aff_club_rider).to_parquet(AFFILIATIONS_PARQUET_FILE, index=False)
+
+
+if __name__ == "__main__":
+    transform()
